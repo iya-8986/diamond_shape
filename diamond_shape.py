cffa6b352e620f70cfaf8b3731bcbbe8a6c1bb1c
Thea <uythea487102@gmail.com>
2024-09-26 19:34:18 +0800

    Changed the author name format in the code

--- diamond_shape.py
+++ diamond_shape.py
@@ -1,4 +1,4 @@
-#author__thea_uy
+#author__uy_thea
 #date__September_26_2024
 
 #Write a Python function named print_diamond that takes an odd integer n as an argument and prints a diamond shape with a width of n using the * character.
